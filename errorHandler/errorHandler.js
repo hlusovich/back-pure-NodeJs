4f2a160b435a4c398a33ac471155c40378120478
hlusovich <bigklu95@gmail.com>
2021-11-23 21:58:17 +0300

    add class for custom error

--- errorHandler/errorHandler.js
+++ errorHandler/errorHandler.js
@@ -1,3 +1,4 @@
+const MyError = require('../MyError/MyError');
 const errorHandler = (error,res)=>{
   if(error instanceof MyError){
       res.writeHead(error.status, {"Content-Type": "application/json"});
